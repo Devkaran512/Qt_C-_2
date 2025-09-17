5477ba497f51ab14b5153fcb085d73907c800046
Devkaran Lodhi <devkaran@gmail.com>
2025-09-17 21:13:47 +0530

    hello dev-1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	char str[]="devkaran";
+	int len=(sizeof(str)/sizeof(str[0]))-1;
+	cout<<len<<endl;
+	cout<<str<<endl;
+	
+	for(int i=0;i<len/2;i++)
+	{
+		char temp[10]=str[i];
+		str[i]=str[len-i-1];
+		str[len-i-1]=temp[i];
+	}
+	
+	cout<<str<<endl;
+	
+	return 0;
+}
